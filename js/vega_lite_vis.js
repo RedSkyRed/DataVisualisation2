73b095e5f684c7f4c2e0bc6a7da2b32e45cbe50c
milfo <lmil0027@student.monash.edu>
2022-10-13 23:54:02 +1100

    Reverted changes

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/flowmap.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/flowmap.json";
 vegaEmbed("#flow_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
