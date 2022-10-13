45e048e8df5cf3cc6ad4821ed4b0ee2c8749682b
milfo <lmil0027@student.monash.edu>
2022-10-14 05:16:33 +1100

    Attempt 1 at implementing the HTML

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,25 @@
 var vg_1 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/flowmap.json";
+var vg_2 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/T1Worlds.json";
+var vg_3 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/gamesplayed.json";
+var vg_4 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/winrate.json";
+var vg_5 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/KDA.json";
+
 vegaEmbed("#flow_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+vegaEmbed("#t1_worlds", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+vegaEmbed("#gamesplayed", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#winrate", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+vegaEmbed("#kda", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
