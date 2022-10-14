1598f4696b28b6d26ca1f257d595f933ac53950e
milfo <lmil0027@student.monash.edu>
2022-10-15 02:22:57 +1100

    MInor change, named a vg badly

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,6 +3,7 @@ var vg_2 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/
 var vg_3 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/gamesplayed.json";
 var vg_4 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/winrate.json";
 var vg_5 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/KDA.json";
+var vg_6 = "https://raw.githubusercontent.com/RedSkyRed/DataVisualisation2/main/js/heatmap.json";
 
 vegaEmbed("#flow_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -21,5 +22,9 @@ vegaEmbed("#winrate", vg_4).then(function(result) {
     }).catch(console.error);
 
 vegaEmbed("#kda", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+vegaEmbed("#heatmap", vg_6).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
